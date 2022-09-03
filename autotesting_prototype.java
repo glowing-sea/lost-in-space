260acc61aa32751ca03946cfa20374078bfba1ac
talon_c8 <albert.yu@anu.edu.au>
2022-09-03 21:45:21 +1000

    .gitlab-ci.yml, autotesting_prototype.java written by Albert Yu

--- autotesting_prototype.java
+++ autotesting_prototype.java
@@ -1,5 +1,11 @@
 public class autotesting_prototype {
 
+    /**
+     * Author: Albert Yu
+     *
+     * Class created to test automated testing.  Initial prototype.
+     * @param args
+     */
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
