f6e2af43edbad8bae5547c9e867b9205c3a08a87
talon_c8 <albert.yu@anu.edu.au>
2022-09-03 22:31:14 +1000

    automated_testing_install.txt, test.bat by Albert Yu

---
+++ test.bat
@@ -0,0 +1,7 @@
+:: Author: Albert Yu
+:: Installation:
+:: - Ensure you rename the classpath parameter to your own personal one.
+:: - Place this file in your GitLab-Runner directory.
+
+java -cp C:\Users\yenga\IdeaProjects\comp2120-2022-group-assignment-2\out\production\comp2120-2022-group-assignment-2 autotesting_prototype
+
