018a5ff84168f6187d084aa60a0276691f82d624
talon_c8 <albert.yu@anu.edu.au>
2022-09-07 04:47:37 +1000

    autotesting_prototype.java by Albert Yu

--- autotesting_prototype.java
+++ autotesting_prototype.java
@@ -1,3 +1,18 @@
+/**
+ * The automated testing prototype works by virtue of three components:
+ *
+ * > .gitlab-ci.yml
+ * > test.bat
+ * > gitlab-runner.exe
+ *
+ * .gitlab-ci.yml specifies the test suite
+ * test.bat executes things
+ * gitlab-runner is the main entity in the background that automates running of the tests
+ *
+ * Everytime we check in things from your IDE to GitLab, test.bat will be run, and the CI/DI pipeline will hold the job, and run it in the pipeline.
+ *
+ * If you have any questions, feel free to shoot me a message. I'll continually maintain and improve this subsystem.
+ */
 public class autotesting_prototype {
 
     /**
