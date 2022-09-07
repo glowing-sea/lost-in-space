6bdfa09fc1c2efeb4f5d0d105f86c6772d599d13
talon_c8 <albert.yu@anu.edu.au>
2022-09-07 22:17:42 +1000

    Changes to EnemyUnitTests.java by Albert Yu
    Actioned Haoting's comments on Issue #12.

--- JUnit/main/EnemyUnitTests.java
+++ JUnit/main/EnemyUnitTests.java
@@ -4,16 +4,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
+import main.Enemy;
+import main.Location;
+
 /**
  * @Author Albert Yu
  */
 public class EnemyUnitTests {
 
-    main.Enemy c;
+    Enemy c;
 
     @Before
     public void beforeEachTestMethod() {
-        c = new main.Enemy("talon", 100, 5, 5, new main.Location(0, 0), false);
+        c = new Enemy("talon", 100, 5, 5, new Location(0, 0), false);
 
     }
 
