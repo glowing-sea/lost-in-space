c87c38bae8074806b6a1640babcee39b249abda1
talon_c8 <albert.yu@anu.edu.au>
2022-09-08 17:48:47 +1000

    MapUnitTests.java by Albert Yu
    Written to help guide the team.  This unit test class can be copied and improve upon by team members.

---
+++ JUnit/main/MapUnitTests.java
@@ -0,0 +1,35 @@
+package main;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Albert Yu
+ */
+public class MapUnitTests {
+
+    String[] testMap;
+    Map m;
+
+    @Before
+    public void beforeEachTestMethod() {
+        testMap = new String[] {
+                "    -----",
+                "         ",
+                "    +--  ",
+                "    |    ",
+                "    +----",
+                "       A ",
+                "      AHA",
+                "---------",
+                "         "};
+
+        m = new Map(1, testMap, new char[] {'-', '+', '|'});
+    }
+
+    @Test
+    public void testCaseGetMapID() {
+        Assert.assertEquals(1, m.getMapID());
+    }
+}
