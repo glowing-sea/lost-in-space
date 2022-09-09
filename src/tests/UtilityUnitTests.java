df2eae4c439c10efa73456992cd800506bec79d8
talon_c8 <albert.yu@anu.edu.au>
2022-09-09 19:31:19 +1000

    Second draft automated_testing system by Albert Yu
    Commited-by: Albert Yu
    TODO
     > automation not presently working I think. The code needs changes.
      > Action taken to rectify code.
      > Will generate a merge request now in an attempt to gain team consensus on correctness.

--- src/tests/UtilityUnitTests.java
+++ src/tests/UtilityUnitTests.java
@@ -1,12 +1,16 @@
 package tests;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import utility.Utility;
 
+import java.io.File;
+
 public class UtilityUnitTests {
 
     static String fn;
+    private static final String ROOT_DIR = "C:/temp";
 
     @BeforeClass
     public static void beforeEachTestMethod() {
@@ -15,6 +19,9 @@ public class UtilityUnitTests {
 
     @Test
     public void testWriteToFile() {
-        Utility.writeToFile(fn);
+
+        Utility.writeToFile(fn, "foo foo foo");
+        File f = new File(ROOT_DIR + "/" + fn);
+        Assert.assertTrue(f.exists());
     }
 }
--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 public class Utility {
     private static final String ROOT_DIR = "C:/temp";
 
-    public static void writeToFile(String fileName) {
+    public static void writeToFile(String fileName, String data) {
 
         try {
             FileWriter fw = new FileWriter(ROOT_DIR + "/" + fileName);
 
-            fw.write("foo foo foo");
+            fw.write(data);
 
             fw.close();
         }
