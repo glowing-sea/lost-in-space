ec01566dc74c91947f9f6d6e247d4a7390b09577
talon_c8 <albert.yu@anu.edu.au>
2022-09-11 05:51:44 +1000

    Seventh draft automated_testing system by Albert Yu
    Commited-by: Albert Yu
    
    Push again. I don't believe the last push worked.

--- autotesting_prototype.java
+++ autotesting_prototype.java
@@ -19,6 +19,7 @@ public class autotesting_prototype {
     /**
      * Author: Albert Yu
      *
+     *
      * Class created to test automated testing.  Initial prototype.
      * @param args
      */
